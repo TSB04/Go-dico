88cddf02304d1b05f79254a791c10e2809774987
Thierno Sadou Barry <thierno-sdaou.barry@estiam.com>
2023-12-19 16:55:14 +0100

    cleaned..

--- dico/dicorep/dico.go
+++ dico/dicorep/dico.go
@@ -20,14 +20,12 @@ func (d *Dico) Add(key string, value string) {
 }
 
 // get a word and its definition from the dictionary with the key word
-
 func (d *Dico) Get(word string) {
 	if d.couple[word] != "" {
 		fmt.Println(word +" : " + d.couple[word])
 	} else {
 		fmt.Println("This word doesn't exist")
 	}
-	
 }
 
 // remove a word and its definition from the dictionary 
@@ -38,12 +36,9 @@ func (d *Dico) Remove(word string) {
 	} else {
 		fmt.Println("This word doesn't exist")
 	}
-	
 }
 
-
 // display all words and definitions from the dictionary
-
 func (d *Dico) Display() {
 	for key, value := range d.couple {
 		fmt.Println(key + " : " + value)
--- dico/main.go
+++ dico/main.go
@@ -5,33 +5,6 @@ import (
 )
 
 func main() {
-
-	// // Créez un nouveau dictionnaire vide.
-	// m := make(map[string]string)
-
-	// // Ajoutez quelques mots et définitions au dictionnaire.
-	// m["golang"] = "Le meilleur langage de programmation du monde"
-	// m["rust"] = "Un langage de programmation système"
-	// m["html"] = "Un langage de programmation de balisage utilisé pour créer des pages Web"
-	// m["java"] = "Un langage de programmation orienté objet"
-	// m["c"] = "Un langage de programmation procédural"
-
-	// //call function ---------------------------------------------------------
-	// // add a word and its definition to the dictionary
-	// add(m, "python", "Un langage de programmation interprété")
-
-	// // entrez un mot et affichez sa définition.
-	// get(m, "golang")
-
-	// // Supprimez un mot du dictionnaire.
-	// remove(m, "java")
-
-	// // Affichez tous les mots et définitions du dictionnaire.
-	// display(m)
-	// //---------------------------------------------------------
-
-	//call method ---------------------------------------------------------
-	// add a word and its definition to the dictionary
 	
 	d := dicorep.New()
 
@@ -53,33 +26,4 @@ func main() {
 	// Affichez tous les mots et définitions du dictionnaire.
 	d.Display()
 	println("---------------------------------------------------------")
-
-
 }
-
-// // function ---------------------------------------------------------
-// // add a word and its definition to the dictionary
-// func add(m map[string]string, key string, value string) {
-// 	m[key] = value
-// }
-
-// func get(m map[string]string, key string) {
-// 	// print key value
-// 	fmt.Println(m[key])
-// }
-
-// func remove(m map[string]string, key string) {
-// 	delete(m, key)
-// }
-
-// func display(m map[string]string) {
-// 	//list
-// 	l := list.New()
-// 	for key, value := range m {
-// 		l.PushBack(key)
-// 		l.PushBack(value)
-// 	}
-// 	for e := l.Front(); e != nil; e = e.Next() {
-// 		fmt.Println(e.Value)
-// 	}
-// }
