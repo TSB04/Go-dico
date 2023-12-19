1f2d26152e8fdfcd9f19fd08574725bfa0871e39
Thierno Sadou Barry <thierno-sdaou.barry@estiam.com>
2023-12-19 15:08:25 +0100

    add methods

--- dico/main.go
+++ dico/main.go
@@ -5,6 +5,12 @@ import (
 	"fmt"
 )
 
+// dico structure
+type dico struct {
+	key   string
+	value string
+}
+
 func main() {
 
 	// Créez un nouveau dictionnaire vide.
@@ -17,6 +23,7 @@ func main() {
 	m["java"] = "Un langage de programmation orienté objet"
 	m["c"] = "Un langage de programmation procédural"
 
+	//call function ---------------------------------------------------------
 	// add a word and its definition to the dictionary
 	add(m, "python", "Un langage de programmation interprété")
 
@@ -28,17 +35,35 @@ func main() {
 
 	// Affichez tous les mots et définitions du dictionnaire.
 	display(m)
+	//---------------------------------------------------------
+
+	//call method ---------------------------------------------------------
+	// add a word and its definition to the dictionary
+	d := dico{}
+	d.add("python", "Un langage de programmation interprété")
+
+	// entrez un mot et affichez sa définition.
+	d.get("golang")
+
+	// Supprimez un mot du dictionnaire.
+	d.remove("java")
+
+	// Affichez tous les mots et définitions du dictionnaire.
+	d.display()
+	//---------------------------------------------------------
+
 }
+
+// function ---------------------------------------------------------
 
 // add a word and its definition to the dictionary
 func add(m map[string]string, key string, value string) {
 	m[key] = value
 }
 
-func get(m map[string]string, key string,) {
+func get(m map[string]string, key string) {
 	// print key value
 	fmt.Println(m[key])
-
 }
 
 func remove(m map[string]string, key string) {
@@ -56,3 +81,34 @@ func display(m map[string]string) {
 		fmt.Println(e.Value)
 	}
 }
+
+//---------------------------------------------------------
+
+// method ---------------------------------------------------------
+// add a word and its definition to the dictionary
+func (d *dico) add(key string, value string) {
+	d.key = key
+	d.value = value
+}
+
+// get a word and its definition from the dictionary with the key word
+func (d *dico) get(word string) {
+	if d.key == word {
+		fmt.Println(d.key + " : " + d.value)
+	}
+}
+
+// remove a word and its definition from the dictionary with the key word
+func (d *dico) remove(word string) {
+	if d.key == word {
+		d.key = ""
+		d.value = ""
+	}
+}
+
+// display all words and definitions from the dictionary
+func (d *dico) display() {
+	fmt.Println(d.key + " : " + d.value)
+}
+
+//---------------------------------------------------------
