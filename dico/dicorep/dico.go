98971b9b9851e622f2d8c598af1ce1305e09c9b3
Thierno Sadou Barry <thierno-sdaou.barry@estiam.com>
2023-12-20 14:14:25 +0100

    updated dico to save data in text file

--- dico/dicorep/dico.go
+++ dico/dicorep/dico.go
@@ -1,38 +1,92 @@
 package dicorep
 
-import "fmt"
-
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 // dico structure
 type Dico struct {
-	couple map[string]string
+	filename string
+	couple   map[string]string
 }
 
-// create a new empty dictionary
-func New() *Dico {
-	return &Dico{couple: make(map[string]string)}
+func New(filename string) *Dico {
+	d := &Dico{
+		filename: filename,
+		couple:   make(map[string]string),
+	}
+	d.loadFromFile()
+	return d
 }
 
-// add a word and its definition to the dictionary
+func (d *Dico) loadFromFile() {
+	file, err := os.Open(d.filename)
+	if err != nil {
+		return // File doesn't exist or error reading, so an empty map will be initialized
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		parts := strings.Split(line, ":")
+		if len(parts) == 2 {
+			key := strings.TrimSpace(parts[0])
+			value := strings.TrimSpace(parts[1])
+			d.couple[key] = value
+		}
+	}
+}
+
+func (d *Dico) saveToFile() error {
+	file, err := os.Create(d.filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	writer := bufio.NewWriter(file)
+	for key, value := range d.couple {
+		_, err := fmt.Fprintf(writer, "%s : %s\n", key, value)
+		if err != nil {
+			return err
+		}
+	}
+	return writer.Flush()
+}
+
 func (d *Dico) Add(key string, value string) {
 	d.couple[key] = value
+	err := d.saveToFile()
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
 	fmt.Println(key + " has been added")
 }
+
+// Other methods (Get, Remove, Display) remain unchanged...
 
 // get a word and its definition from the dictionary with the key word
 func (d *Dico) Get(word string) {
 	if d.couple[word] != "" {
-		fmt.Println(word +" : " + d.couple[word])
+		fmt.Println(word + " : " + d.couple[word])
 	} else {
 		fmt.Println("This word doesn't exist")
 	}
 }
 
-// remove a word and its definition from the dictionary 
+// remove a word and its definition from the dictionary
 func (d *Dico) Remove(word string) {
 	if d.couple[word] != "" {
 		delete(d.couple, word)
-		fmt.Println(word + " has been deleted")
+		err := d.saveToFile()
+		if err != nil {
+			fmt.Println("Error:", err)
+		}
+		fmt.Println(word + " has been removed")
 	} else {
 		fmt.Println("This word doesn't exist")
 	}
@@ -44,4 +98,3 @@ func (d *Dico) Display() {
 		fmt.Println(key + " : " + value)
 	}
 }
-
--- dico/main.go
+++ dico/main.go
@@ -5,14 +5,13 @@ import (
 )
 
 func main() {
-	
-	d := dicorep.New()
+
+	d := dicorep.New("dico.txt")
 
 	d.Add("golang", "Le meilleur langage de programmation du monde")
 	d.Add("rust", "Un langage de programmation système")
 	d.Add("html", "Un langage de programmation de balisage utilisé pour créer des pages Web")
 	d.Add("java", "Un langage de programmation orienté objet")
-
 	println("---------------------------------------------------------")
 
 	// entrez un mot et affichez sa définition.
