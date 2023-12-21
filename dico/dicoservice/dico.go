de25572a5cd22bb680495940e66ff1911936030c
Thierno Sadou Barry <thierno-sdaou.barry@estiam.com>
2023-12-21 13:25:31 +0100

    ...

--- dico/dicoservice/dico.go
+++ dico/dicoservice/dico.go
@@ -20,26 +20,26 @@ func NewDicoService(filename string) *DicoService {
 
 // AddWordHandler handles the addition of a word and its definition.
 func (ds *DicoService) AddWordHandler(w http.ResponseWriter, r *http.Request) {
-    log.Println("Received request to add word")
+	log.Println("Received request to add word")
 
-    // Define a struct that matches the expected JSON structure
-    type RequestData struct {
-        Key   string `json:"key"`
-        Value string `json:"value"`
-    }
+	// Define a struct that matches the expected JSON structure
+	type RequestData struct {
+		Key   string `json:"key"`
+		Value string `json:"value"`
+	}
 
-    // Decode the request body into the struct
-    decoder := json.NewDecoder(r.Body)
-    var requestData RequestData
-    err := decoder.Decode(&requestData)
-    if err != nil {
-        http.Error(w, "Invalid request payload", http.StatusBadRequest)
-        return
-    }
+	// Decode the request body into the struct
+	decoder := json.NewDecoder(r.Body)
+	var requestData RequestData
+	err := decoder.Decode(&requestData)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
 
-    // Use the data from the request in your logic
-    ds.Dico.Add(requestData.Key, requestData.Value)
-    w.WriteHeader(http.StatusCreated)
+	// Use the data from the request in your logic
+	ds.Dico.Add(requestData.Key, requestData.Value)
+	w.WriteHeader(http.StatusCreated)
 }
 
 // GetWordHandler handles getting the definition of a word.
@@ -65,7 +65,6 @@ func (ds *DicoService) GetAllWordsHandler(w http.ResponseWriter, r *http.Request
 	log.Println("Received request to get all words")
 	json.NewEncoder(w).Encode(ds.Dico.Couple)
 }
-
 
 // RemoveWordHandler handles the removal of a word and its definition.
 func (ds *DicoService) RemoveWordHandler(w http.ResponseWriter, r *http.Request) {
--- dico/main.go
+++ dico/main.go
@@ -2,21 +2,33 @@ package main
 
 import (
 	"dico/dicoservice"
+	"fmt"
 	"log"
 	"net/http"
 )
 
-func main() {
-	// Create a DicoService
-	dicoService := dicoservice.NewDicoService("dico.txt")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!"+"\n"+"Available endpoints:"+"\n"+"/add"+"\n"+"/get"+"\n"+"/list"+"\n"+"/remove")
+	fmt.Println("Endpoint Hit: homePage")
+}
+
+func handleRequests(ds *dicoservice.DicoService) {
+
+	// Call homePage at root endpoint
+	http.HandleFunc("/", homePage)
 
 	// Setup HTTP handlers
-	http.HandleFunc("/add", dicoService.AddWordHandler)
-	http.HandleFunc("/get", dicoService.GetWordHandler)
-	http.HandleFunc("/list", dicoService.GetAllWordsHandler)
-	http.HandleFunc("/remove", dicoService.RemoveWordHandler)
+	http.HandleFunc("/add", ds.AddWordHandler)
+	http.HandleFunc("/get", ds.GetWordHandler)
+	http.HandleFunc("/list", ds.GetAllWordsHandler)
+	http.HandleFunc("/remove", ds.RemoveWordHandler)
 
-	// Start the server
 	log.Fatal(http.ListenAndServe(":8081", nil))
+}
 
+func main() {
+
+	dicoService := dicoservice.NewDicoService("dico.txt")
+
+	handleRequests(dicoService)
 }
